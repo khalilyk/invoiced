849765711027356a41b7cb844bfb0f89e16ac725
Khalil Khouri <khalilykhouri@gmail.com>
2026-04-19 13:22:20 +1000

    changed start text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <p className="text-muted-foreground">Start somewhere.</p>
+      <p className="text-muted-foreground">Hello world</p>
     </main>
   )
 }
